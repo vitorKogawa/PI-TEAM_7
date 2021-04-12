e706c05ea707ef81f2de116b89b8feaf8bb78582
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-04-12 10:59:19 -0300

    adicionando cors

--- pi_backend/app.js
+++ pi_backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const { sequelize } = require('./connection')
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 sequelize.sync();
 
