f47503500360e70215a1aa8e23ec212f07bcdd1c
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-04-13 10:04:29 -0300

    esboço de rotas do usuário

--- pi_backend/app.js
+++ pi_backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { user_router } = require('./routes/users.routes')
 const cors = require('cors');
 const { sequelize } = require('./connection')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(user_router);
 sequelize.sync();
 
 module.exports = { app: app };
--- pi_backend/server.js
+++ pi_backend/server.js
@@ -4,6 +4,6 @@ const { app } = require('./app')
 const server = express();
 server.use(express.json())
 server.use(app);
-const port = 3000;
+const port = 3333;
 
 server.listen(port, () => console.log(`Servidor rodando em http://127.0.0.1:${port}`))
