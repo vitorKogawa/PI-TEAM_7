1ff03e778979666a9af2681235d9bb009ac6677a
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-06 12:08:40 -0300

    implementando model de usuário

--- pi_backend/src/app.js
+++ pi_backend/src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import { userRouter } from "./routes/users.routes";
-import { authRouter } from './routes/authenticate.routes';
-import { pagamentoRouter } from './routes/pagamento.routes';
-import { tipoPagamentoRouter } from './routes/tipo_pagamento.routes';
-import { jogoRouter } from './routes/jogos.routes';
+import { authRouter } from "./routes/authenticate.routes";
+import { pagamentoRouter } from "./routes/pagamento.routes";
+import { tipoPagamentoRouter } from "./routes/tipo_pagamento.routes";
+import { jogoRouter } from "./routes/jogos.routes";
 import "./database/index";
 
 class App {
@@ -17,6 +18,7 @@ class App {
 
     middlewares() {
         this.server.use(cors());
+        this.server.use(helmet());
         this.server.use(express.json());
     }
 
---
+++ pifrontend/src/app/models/User.ts
@@ -0,0 +1,12 @@
+class User{
+  id?: number;
+  nome?: string;
+  prec?: number;
+  quantidade?: number;
+  descricao?: string;
+  espaco_arm?: number;
+  genero?: string;
+  status?: boolean;
+}
+
+export { User }
