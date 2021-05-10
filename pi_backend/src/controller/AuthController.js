49fc2caeea6be44814891eb2a65bb82940bd5681
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-10 18:28:44 -0300

    colocando ativo e status_adm no payload do token

--- pi_backend/src/controller/AuthController.js
+++ pi_backend/src/controller/AuthController.js
@@ -3,25 +3,25 @@ import User from "../models/tb_usuario";
 import authenticateConfig from "../config/authenticate.config";
 
 class AuthController {
-  async signin(request, response) {
-    const { email, senha } = request.body;
+    async signin(request, response) {
+        const { email, senha } = request.body;
 
-    const userExists = await User.findOne({ where: { email } });
+        const userExists = await User.findOne({ where: { email } });
 
-    if (!userExists) {
-      return response.status(400).json({ message: "Usuário não existe!" });
-    } else {
-      if (!(await userExists.verifyPassword(senha))) {
-        return response.status(400).json({ message: "Dados inválidos." });
-      } else {
-        const user = { id: userExists.id, name: userExists.name };
-        const acessToken = sign(user, authenticateConfig.secret, {
-          expiresIn: authenticateConfig.expiresIn,
-        });
-        return response.status(200).json({ user, token: acessToken });
-      }
+        if (!userExists) {
+            return response.status(400).json({ message: "Usuário não existe!" });
+        } else {
+            if (!(await userExists.verifyPassword(senha))) {
+                return response.status(400).json({ message: "Dados inválidos." });
+            } else {
+                const user = { id: userExists.id, name: userExists.name, ativo: userExists.ativo, status_adm: userExists.status_adm };
+                const acessToken = sign(user, authenticateConfig.secret, {
+                    expiresIn: authenticateConfig.expiresIn,
+                });
+                return response.status(200).json({ user, token: acessToken });
+            }
+        }
     }
-  }
 }
 
 export default new AuthController();
