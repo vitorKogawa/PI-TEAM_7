7f387cf49848934ae76a21d887c88b2e6b632df7
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-10 20:27:43 -0300

    implementando a migrations de remoção da coluna data_pagamento, hora_pagamento e adição da coluna status na tabela pagamento

--- pi_backend/src/models/tb_pagamento.js
+++ pi_backend/src/models/tb_pagamento.js
@@ -1,22 +1,18 @@
 import { Sequelize, Model } from "sequelize";
 
 class Pagamento extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        data_pagamento: Sequelize.DATE,
-        hora_pagamento : Sequelize.DATE,
-      },
-      { sequelize, tableName: "tb_pagamento" }
-    );
-    return this;
-  }
+    static init(sequelize) {
+        super.init({
+            status: Sequelize.BOOLEAN
+        }, { sequelize, tableName: "tb_pagamento" });
+        return this;
+    }
 
-  static associate(models) {
-    this.belongsTo(models.Jogo, { foreignKey: "cod_jogo" });
-    this.belongsTo(models.User, { foreignKey: "cod_usuario" });
-    this.belongsTo(models.TipoPagamento, { foreignKey: "cod_tipo_pagamento" });
-  }
+    static associate(models) {
+        this.belongsTo(models.Jogo, { foreignKey: "cod_jogo" });
+        this.belongsTo(models.User, { foreignKey: "cod_usuario" });
+        this.belongsTo(models.TipoPagamento, { foreignKey: "cod_tipo_pagamento" });
+    }
 }
 
 export default Pagamento;
