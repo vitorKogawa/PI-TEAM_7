86c3c2dd22b58ed135a3675f2794ed2e7fe6c4e0
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-12 19:15:26 -0300

    ajustando service do jogo

--- pifrontend/src/app/components/jogo/jogo.service.ts
+++ pifrontend/src/app/components/jogo/jogo.service.ts
@@ -2,45 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
-import { Jogo } from './jogo.model';
+import { IJogo } from './../../models/IJogo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JogoService {
-  baseUrl = 'http://localhost:3333/jogo'
+  baseUrl = 'http://localhost:3333/jogo';
 
-  constructor(private snackBar: MatSnackBar,
-    private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg:string):void {
-    this.snackBar.open(msg,'X',{
-      duration:3000,
-      horizontalPosition:'right',
-      verticalPosition:'top'
-    })
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
   }
-  //conexao com o backend
-  //cadastro do jogo
-  create(jogo: Jogo):Observable<Jogo> {
-    return this.http.post<Jogo>(this.baseUrl, jogo)
+  create(jogo: IJogo): Observable<IJogo> {
+    return this.http.post<IJogo>(this.baseUrl, jogo);
   }
 
-  //filtrando o jogo
-  read(): Observable<Jogo[]>{
-    return this.http.get<Jogo[]>(this.baseUrl)
+  read(): Observable<IJogo[]> {
+    return this.http.get<IJogo[]>(this.baseUrl);
   }
 
-  //buscando por id
-  readById(id: string): Observable<Jogo> {
-    const url = `${this.baseUrl}/${id}`
-    return this.http.get<Jogo>(url)
+  readById(id: string): Observable<IJogo> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<IJogo>(url);
   }
 
-  //alteração do jogo
-  update(jogo:Jogo): Observable<Jogo> {
-    const url = `${this.baseUrl}/${jogo.id}`
-    return this.http.put<Jogo>(url, jogo)
+  update(jogo: IJogo): Observable<IJogo> {
+    const url = `${this.baseUrl}/${jogo.id}`;
+    return this.http.put<IJogo>(url, jogo);
   }
-
 }
