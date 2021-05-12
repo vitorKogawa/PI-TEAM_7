84bcfac3303901ce964622fd669eb275759b73dd
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-12 10:00:21 -0300

    implementando método create na service de pagamento

--- pifrontend/src/app/views/pagamento/pagamento.service.ts
+++ pifrontend/src/app/views/pagamento/pagamento.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPagamento } from 'src/app/models/IPagamento';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagamentoService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  baseUrl: string = "http://localhost:3333/pagamento";
+
+  create(pagamento: IPagamento): Observable<IPagamento>{
+    return this.httpClient.post<IPagamento>(this.baseUrl, pagamento);
+  }
 }
