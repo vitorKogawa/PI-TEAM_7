8db6cbab0eb7475157abd1aa7bc0ee3ff3d68e37
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-12 11:15:57 -0300

    ajustando a classe do login component

--- pifrontend/src/app/views/login/login.component.ts
+++ pifrontend/src/app/views/login/login.component.ts
@@ -1,34 +1,36 @@
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { LoginService } from './login.service';
+import { IUsuario } from 'src/app/models/IUsuario';
+import { ILogin } from 'src/app/models/ILogin';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
-  templateUrl: 'login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit {
-  // form: FormGroup;
-  email: string = ''
-  senha: string = ''
-  token: string = ''
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) {
-  }
+  constructor(private loginService: LoginService, private router: Router) {}
+  credentials: IUsuario = {
+    email: '',
+    senha: '',
+  };
 
-  ngOnInit(): void { }
+  token: ILogin = {
+    user: {
+      id: 0,
+      ativo: false,
+      status_adm: false,
+    },
+    token: '',
+  };
 
-  submit(): Observable<{ token: string }> {
-    return this.http.post<{ token: string }>('http://127.0.0.1:3333/login', { this.email, this.senha })
-  }
+  ngOnInit(): void {}
+
   login(): void {
-    this.submit().subscribe(token => {
-      console.log(token),
-        this.token = token
-    })
+    this.loginService.submit(this.credentials).subscribe((token) => {
+      this.token = token,
+      sessionStorage.setItem('token', JSON.stringify(token)),
+      this.router.navigate(['/home'])
+    });
   }
 }
