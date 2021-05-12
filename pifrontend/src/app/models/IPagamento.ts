448d77b59b2c3f3c145781cd29f401b352d340c4
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-12 10:00:38 -0300

    implemetando a interface de pagamento

---
+++ pifrontend/src/app/models/IPagamento.ts
@@ -0,0 +1,9 @@
+export interface IPagamento{
+  id?: number,
+  cod_usuario?: number,
+  cod_jogo?: number,
+  cod_tipo_pagamento?: number,
+  status?: boolean,
+  createdAt: Date,
+  updatedAt: Date
+}
