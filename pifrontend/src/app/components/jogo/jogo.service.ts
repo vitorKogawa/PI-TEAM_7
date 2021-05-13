70e7ea383c50572e74fc8e2ab8fa7284bd93c6a1
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-13 10:07:08 -0300

    ajustes no service do jogo

--- pifrontend/src/app/components/jogo/jogo.service.ts
+++ pifrontend/src/app/components/jogo/jogo.service.ts
@@ -25,7 +25,8 @@ export class JogoService {
 
   read(): Observable<IJogo[]> {
     return this.http.get<IJogo[]>(this.baseUrl);
-  
+  }
+
   readById(id: string | null): Observable<IJogo> {
     const url = `${this.baseUrl}/${id}`
     return this.http.get<IJogo>(url);
@@ -35,7 +36,7 @@ export class JogoService {
     const url = `${this.baseUrl}/${jogo.id}`;
     return this.http.put<IJogo>(url, jogo);
   }
-  
+
   delete(id: string | undefined):Observable<IJogo> {
     const url = `${this.baseUrl}/${id}`
     return this.http.delete<IJogo>(url);
