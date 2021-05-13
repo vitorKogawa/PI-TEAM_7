68548d7676840a45a4b8cea6ae05e33a0ddb89e6
Bruna Pianco <bruna.magalhaes02@hotmail.com>
2021-05-13 17:09:28 -0300

    upload

--- pifrontend/src/app/components/jogo/jogo-create/jogo-create.component.ts
+++ pifrontend/src/app/components/jogo/jogo-create/jogo-create.component.ts
@@ -23,7 +23,7 @@ export class JogoCreateComponent implements OnInit {
     imageUrl: '',
   };
 
-  files:Set<File> | undefined
+  files: File | undefined
 
   constructor(private jogoService: JogoService, private router: Router, private http: HttpClient) {
   }
@@ -42,19 +42,28 @@ export class JogoCreateComponent implements OnInit {
   }
 
   onUpload() {
-    if (this.files && this.files.size > 0){
-      this.jogoService.upload(this.files,'') .subscribe
-      (response => console.log('Upload Concluído'));
-    }
+    //if (this.files && this.files.size > 0){
+      //this.jogoService.upload(this.files,'') .subscribe
+      //(response => console.log('Upload Concluído'));
+    //}
+    console.log(this.files)
   }
 
   createJogo(): void {
+    const fd = new FormData();
+    fd.set("name", this.jogo.nome)
+    fd.set("preco", this.jogo.preco)
+    fd.set("descricao", this.jogo.descricao)
+    fd.set("espaco_arm",this.jogo.espaco_arm)
+    fd.set("genero",this.jogo.genero)
+    fd.set("status",this.jogo.status)
+    fd.append("file",this.files.slice(),file.name)
+
     this.jogoService.create(this.jogo).subscribe(() => {
       this.jogoService.showMessage('Jogo cadastrado com sucesso!');
       this.router.navigate(['/jogos']);
     });
   }
-
   cancel(): void {
     this.router.navigate(['/jogos']);
   }
