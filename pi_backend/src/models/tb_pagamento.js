0f92e1c30984667a256b571a9bf044f838073c70
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-13 09:56:50 -0300

    ajustes das rotas e model de pagamento

--- pi_backend/src/models/tb_pagamento.js
+++ pi_backend/src/models/tb_pagamento.js
@@ -3,18 +3,15 @@ import { Sequelize, Model } from "sequelize";
 class Pagamento extends Model {
     static init(sequelize) {
         super.init({
-            status: Sequelize.BOOLEAN,
-            cod_usuario: Sequelize.INTEGER,
-            cod_jogo: Sequelize.INTEGER,
-            cod_tipo_pagamento: Sequelize.INTEGER
+            status: Sequelize.BOOLEAN
         }, { sequelize, tableName: "tb_pagamento" });
         return this;
     }
 
     static associate(models) {
         Pagamento.belongsTo(models.User, { foreignKey: "cod_usuario" });
-        Pagamento.hasOne(models.Jogo, { foreignKey: "cod_jogo" });
-        Pagamento.hasOne(models.TipoPagamento, { foreignKey: "cod_tipo_pagamento" });
+        Pagamento.belongsTo(models.Jogo, { foreignKey: "cod_jogo" });
+        Pagamento.belongsTo(models.TipoPagamento, { foreignKey: "cod_tipo_pagamento" });
     }
 }
 
--- pi_backend/src/routes/pagamento.routes.js
+++ pi_backend/src/routes/pagamento.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import PagamentoController from "../controller/PagamentoController"
 
-const pagamentoRouter = Router ();
+const pagamentoRouter = Router();
 pagamentoRouter.post("/pagamento", PagamentoController.create);
-pagamentoRouter.get("/pagamento/:id", PagamentoController.findByPk);
-pagamentoRouter.delete("/pagamento/:id", PagamentoController.deleteByPk);
-pagamentoRouter.get("/pagamento/usuario/:id", PagamentoController.findAllByUserPk);
 pagamentoRouter.get("/pagamento", PagamentoController.findAll);
+pagamentoRouter.get("/pagamento/:id", PagamentoController.findByPk);
+pagamentoRouter.get("/pagamento/usuario/:id", PagamentoController.findAllByUserPk);
+pagamentoRouter.delete("/pagamento/:id", PagamentoController.deleteByPk);
 //pagamento_router.put("/pagamento/:id", pagamento_controller.update);
 
 export { pagamentoRouter };
